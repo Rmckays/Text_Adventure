0274763b763e6a53e728546982ca09f1ed8e2c8c
Rustyn Simmons <Rustyn@rustynsimmons.com>
2020-01-30 20:49:11 -0700

    Add additional states. Update function now responds to user input and updates state.

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -6,16 +6,32 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+    State[] nextStates;
     
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "This is now the new text";
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        nextStates = state.GetNextStates();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+            textComponent.text = state.GetStateStory();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            state = nextStates[1];
+            textComponent.text = state.GetStateStory();
+        }
     }
 }
